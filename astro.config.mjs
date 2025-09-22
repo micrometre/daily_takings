abf47ede2265ee62f0c65c6451d96ddb703caa4c
Henok Wehibe <henokwehibe@gmail.com>
2025-09-22 22:16:03 +0100

    removed astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
